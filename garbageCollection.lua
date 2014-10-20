a6e329e7d35dadcd05ad69c43f8ed5a14158e970
aenerv7 <aenerv7@gmail.com>
2014-10-20 21:11:11 +0800

    Update garbageCollection.lua

--- garbageCollection.lua
+++ garbageCollection.lua
@@ -1,9 +1,5 @@
---[[
-来源：NGA，但无法找具体的地址了...
-]]--
-
 --脱战回收内存
-local b = CreateFrame("Frame")
-b:RegisterEvent("PLAYER_REGEN_ENABLED")
-b:SetScript("OnEvent", function() _G.collectgarbage("collect") end)
-frame:SetScript("OnEvent", eventHandler)
+
+local gc = CreateFrame("Frame","GCFrame")
+gc:RegisterEvent("PLAYER_REGEN_ENABLED")
+gc:SetScript("OnEvent", function() _G.collectgarbage("collect") end)
