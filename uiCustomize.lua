933e14b8925def991ffcb4680cf33cc6d071b253
aenerv7 <aenerv7@gmail.com>
2015-12-21 11:51:03 +0800

    Update uiCustomize.lua

--- uiCustomize.lua
+++ uiCustomize.lua
@@ -23,3 +23,6 @@ PlayerHitIndicator:SetText(nil)
 PlayerHitIndicator.SetText = function() end
 PetHitIndicator:SetText(nil)
 PetHitIndicator.SetText = function() end
+
+--隐藏快捷键名称
+local r={"MultiBarBottomLeft", "MultiBarBottomRight", "Action", "MultiBarLeft", "MultiBarRight"} for b=1,#r do for i=1,12 do _G[r[b].."Button"..i.."HotKey"]:SetAlpha(0) end end
